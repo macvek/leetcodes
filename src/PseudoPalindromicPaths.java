d177fe6728aeecdfc200fb87483703e237b67846
Maciej Aleksandrowicz <macvek@gmail.com>
2022-09-15 17:23:08 +0200

    rewritten to linked list

--- src/PseudoPalindromicPaths.java
+++ src/PseudoPalindromicPaths.java
@@ -2,31 +2,43 @@ import java.util.*;
 
 public class PseudoPalindromicPaths {
 
+    static class Payload {
+        Payload prev;
+        Integer value;
 
-    public int pseudoPalindromicPaths (TreeNode root) {
-        return walkWithPayload(root, new ArrayList<>());
+        public Payload(Payload prev, Integer value) {
+            this.prev = prev;
+            this.value = value;
+        }
+
     }
 
-    public int walkWithPayload(TreeNode root, List<Integer> trail) {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        return walkWithPayload(root, new Payload(null, null));
+    }
+
+    public int walkWithPayload(TreeNode root, Payload trail) {
         if (root == null) {
             return 0;
         }
-        ArrayList<Integer> copied = new ArrayList<>(trail);
-        copied.add(root.val);
+        Payload p = new Payload(trail, root.val);
 
         if (root.left == null && root.right == null) {
-            return applies(copied);
+            return applies(p);
         }
         else {
-            return walkWithPayload(root.left, copied) + walkWithPayload(root.right, copied);
+            return walkWithPayload(root.left, p) + walkWithPayload(root.right, p);
         }
     }
 
-    public int applies(List<Integer> trail) {
+    public int applies(Payload p) {
         HashMap<Integer, Integer> counts = new HashMap<>();
-        for (Integer each : trail) {
+        Payload next = p;
+        while(next != null && next.value != null) {
+            var each = next.value;
             counts.putIfAbsent(each, 0);
             counts.put(each, counts.get(each)+1);
+            next = next.prev;
         }
 
         int passed = 0;
